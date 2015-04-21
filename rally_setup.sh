cd6fbe531e68170e7a3eb5105198f6bd406c4241
Swapnil Kulkarni <me@coolsvap.net>
2015-04-21 21:04:46 +0530

    Update rally_setup.sh

--- rally_setup.sh
+++ rally_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://git.openstack.org/openstack-dev/devstack
-git clone https://github.com/stackforge/rally
+git clone https://github.com/openstack/rally.git
 cp rally/contrib/devstack/lib/rally devstack/lib/
 cp rally/contrib/devstack/extras.d/70-rally.sh devstack/extras.d/
 cp localrc devstack/
